c043cf9393c24c34abcce1ff25f5c1099c5ba8fd
Jossue Stuff <ajsilesleiva@gmail.com>
2024-11-06 15:16:09 -0600

    Create script.js

---
+++ script.js
@@ -0,0 +1,25 @@
+document.addEventListener('DOMContentLoaded', function() {
+    fetch('productos.csv')
+        .then(response => response.text())
+        .then(data => {
+            const productos = Papa.parse(data, { header: true }).data;
+            mostrarProductos(productos);
+        });
+});
+
+function mostrarProductos(productos) {
+    const catalogo = document.getElementById('catalogo');
+    productos.forEach(producto => {
+        const div = document.createElement('div');
+        div.classList.add('producto');
+        
+        div.innerHTML = `
+            <img src="${producto.Imagen}" alt="${producto.Nombre}">
+            <h2>${producto.Nombre}</h2>
+            <p class="sku">SKU: ${producto.SKU}</p>
+            <p class="precio">C$ ${producto.Precio}</p>
+        `;
+        
+        catalogo.appendChild(div);
+    });
+}
